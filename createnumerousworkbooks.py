b7452012a5a44e36c940e8f6cb4630cebb6a46d1
Jay <115070244+jaychow-DataScientist@users.noreply.github.com>
2022-10-08 12:36:48 +0800

    create numerous workbooks to store similar data
    done with a second and save time to create one by one

---
+++ createnumerousworkbooks.py
@@ -0,0 +1,13 @@
+import xlwings as xw
+
+# create empty workbooks to store similar data
+def createNumeriousWorkBooks():
+    app = xw.App(visible=False, add_book=False)
+# create workbooks from 2017 - 2022
+    years = list(range (2017,2022))
+
+    for year in years:
+        workbook = app.books.add()
+        workbook.save(f"./{year}.xlsx")
+
+createNumeriousWorkBooks()
